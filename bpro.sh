a0fb5568b02eac428f4369ebc3464b04284520ea
Scott M Anderson <smanders@sdl.usu.edu>
2020-08-31 10:54:12 -0600

    consolidate shell.sh, build.sh into bpro.sh

--- bpro.sh
+++ bpro.sh
@@ -4,15 +4,25 @@ gtag=`git describe --tags`
 if [ -n "$(git status --porcelain)" ]; then
   gtag=${gtag}-dr
 fi
+mount=$HOME
+cmd=shell
+if [ -d "$1" ]; then
+  mount=$1
+  if [ -n "$2" ]; then
+    cmd=$2
+  fi
+elif [ -n "$1" ]; then
+  cmd=$1
+fi
 docker container run \
   --volume=$(pwd)/image/scripts:/scripts \
-  --volume=$1:/srcdir \
+  --volume=$mount:/srcdir \
   --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
   --volume="$HOME/.ssh:/home/${USER}/.ssh" \
   --env="DISPLAY" \
   --net=host \
   --user=$(id -u ${USER}):$(id -g ${USER}) \
   --hostname=buildpro_${gtag} \
-  --name=buildpro_shell \
+  --name=buildpro_${cmd} \
   --rm -it buildpro:${gtag} \
-  shell
+  ${cmd}
--- image/scripts/entry.sh
+++ image/scripts/entry.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 cd /srcdir
 if [ $1 = "shell" ]; then
-  echo "starting bash shell"
   /bin/bash
 elif [ $1 = "build" ]; then
   echo "performing software build"
