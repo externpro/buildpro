9ac5da2a2e4b23a2e535b83954506eecd61def45
Scott M Anderson <smanders@sdl.usu.edu>
2020-09-16 11:26:39 -0600

    build multiple docker images
    * also use -bld to differentiate between -bld (build) and -run (for runtime)

--- image/buildDockerImage.sh
+++ image/buildDockerImage.sh
@@ -4,10 +4,13 @@ gtag=`git describe --tags`
 if [ -n "$(git status --porcelain)" ]; then
   gtag=${gtag}-dr
 fi
-time docker image build \
-  --network=host \
-  --build-arg USERNAME=${USER} \
-  --build-arg USERID=$(id -u ${USER}) \
-  --file centos6.dockerfile \
-  --tag buildpro:${gtag} .
+for img in centos6-bld centos7-bld
+do
+  time docker image build \
+    --network=host \
+    --build-arg USERNAME=${USER} \
+    --build-arg USERID=$(id -u ${USER}) \
+    --file ${img}.dockerfile \
+    --tag buildpro/${img}:${gtag} .
+done
 docker image ls
