4cede9f11ca00ec9bae48259bd1e0cba650d71ad
Scott M Anderson <smanders@sdl.usu.edu>
2020-09-16 15:03:49 -0600

    bpro.sh: run graphical app in container on remote server
    * -x option
    * NOTE: -x must be last argument (or at least after -t)
      * if -x is before -t then CONTAINER_HOSTNAME is incorrect
    * known limitation: socat dies when x application is done
      * example: run xeyes once in container, but second time won't work
    * https://blog.yadutaf.fr/2017/09/10/running-a-graphical-app-in-a-docker-container-on-a-remote-server/
    * DISPLAY files created in .display, which has been added to .gitignore

--- bpro.sh
+++ bpro.sh
@@ -8,7 +8,8 @@ if [ -n "$(git status --porcelain)" ]; then
   TAG=${TAG}-dr
 fi
 CONTAINER_HOSTNAME=buildpro_${TAG}
-while getopts ":c:m:r:t:" opt
+XARG="--volume=/tmp/.X11-unix:/tmp/.X11-unix --env=DISPLAY=${DISPLAY}"
+while getopts ":c:m:r:t:x" opt
 do
   case ${opt} in
     c )
@@ -24,6 +25,20 @@ do
       TAG=$OPTARG
       CONTAINER_HOSTNAME=buildpro_${TAG}
       ;;
+    x )
+      CONTAINER_DISPLAY="0"
+      DISPLAY_NUM=$(echo $DISPLAY | cut -d. -f1 | cut -d: -f2)
+      AUTH_COOKIE=$(xauth list | grep "^$(hostname)/unix:${DISPLAY_NUM} " | awk '{print $3}')
+      rm -rf .display
+      mkdir -p .display/socket
+      touch .display/Xauthority
+      xauth -f .display/Xauthority add ${CONTAINER_HOSTNAME}/unix:${CONTAINER_DISPLAY} \
+        MIT-MAGIC-COOKIE-1 ${AUTH_COOKIE}
+      socat TCP4:localhost:60${DISPLAY_NUM} UNIX-LISTEN:.display/socket/X${CONTAINER_DISPLAY} &
+      XARG="--volume=$(pwd)/.display/socket:/tmp/.X11-unix \
+        --volume=$(pwd)/.display/Xauthority:/home/${USER}/.Xauthority \
+        --env=DISPLAY=:${CONTAINER_DISPLAY}" # 172.17.0.1
+      ;;
     \? )
       echo "Invalid option: $OPTARG" 1>&2
       exit
@@ -38,10 +53,8 @@ shift $((OPTIND -1))
 docker container run \
   --volume=$(pwd)/image/scripts:/scripts \
   --volume=$MOUNT:/srcdir \
-  --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
   --volume="$HOME/.ssh:/home/${USER}/.ssh" \
-  --env="DISPLAY" \
-  --net=host \
+  ${XARG} \
   --user=$(id -u ${USER}):$(id -g ${USER}) \
   --hostname=${CONTAINER_HOSTNAME} \
   --rm -it ${REPO}:${TAG} \
