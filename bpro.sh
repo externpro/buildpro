59419c3bb2c26b7d82f4a28775a9b18ae36152f7
Scott M Anderson <smanders@sdl.usu.edu>
2020-09-16 12:29:44 -0600

    bpro.sh: getopts

--- bpro.sh
+++ bpro.sh
@@ -1,19 +1,40 @@
 #!/usr/bin/env bash
 cd "$( dirname "$0" )"
-GTAG=`git describe --tags`
-if [ -n "$(git status --porcelain)" ]; then
-  GTAG=${GTAG}-dr
-fi
-MOUNT=$HOME
 CMD=shell
-if [ -d "$1" ]; then
-  MOUNT=$1
-  if [ -n "$2" ]; then
-    CMD=$2
-  fi
-elif [ -n "$1" ]; then
-  CMD=$1
+MOUNT=$HOME
+REPO=buildpro/centos6-bld
+TAG=`git describe --tags`
+if [ -n "$(git status --porcelain)" ]; then
+  TAG=${TAG}-dr
 fi
+CONTAINER_HOSTNAME=buildpro_${TAG}
+while getopts ":c:m:r:t:" opt
+do
+  case ${opt} in
+    c )
+      CMD=$OPTARG
+      ;;
+    m )
+      MOUNT=$OPTARG
+      ;;
+    r )
+      REPO=$OPTARG
+      ;;
+    t )
+      TAG=$OPTARG
+      CONTAINER_HOSTNAME=buildpro_${TAG}
+      ;;
+    \? )
+      echo "Invalid option: $OPTARG" 1>&2
+      exit
+      ;;
+    : )
+      echo "Invalid option: $OPTARG requires an argument" 1>&2
+      exit
+      ;;
+  esac
+done
+shift $((OPTIND -1))
 docker container run \
   --volume=$(pwd)/image/scripts:/scripts \
   --volume=$MOUNT:/srcdir \
@@ -22,7 +43,7 @@ docker container run \
   --env="DISPLAY" \
   --net=host \
   --user=$(id -u ${USER}):$(id -g ${USER}) \
-  --hostname=buildpro_${GTAG} \
+  --hostname=${CONTAINER_HOSTNAME} \
   --name=buildpro_${CMD} \
-  --rm -it buildpro:${GTAG} \
+  --rm -it ${REPO}:${TAG} \
   ${CMD}
