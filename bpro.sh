610de9bc423d99faf2be80298e1e81f5ba3b7791
Scott M Anderson <smanders@sdl.usu.edu>
2020-09-17 18:05:27 -0600

    bpro.sh: run graphical app in container on remote server
    * fixes limitation there was with socat solution https://github.com/smanders/buildpro/commit/4cede9f11ca00ec9bae48259bd1e0cba650d71ad
    * requires 'X11UseLocahost no' in /etc/ssh/sshd_config
      * sudo systemctl restart ssh
    * https://stackoverflow.com/questions/48235040/run-x-application-in-a-docker-container-reliably-on-a-server-connected-via-ssh-w/48235281#48235281
    
    X11 forwarding
    * https://stackoverflow.com/questions/33720436/docker-to-run-x-applications-while-connected-through-ssh
    * https://unix.stackexchange.com/questions/12755/how-to-forward-x-over-ssh-to-run-graphics-applications-remotely
    * https://stackoverflow.com/questions/784404/how-can-i-specify-a-display/1210332#1210332
    
    get your docker host's IP address
    * https://nickjanetakis.com/blog/docker-tip-65-get-your-docker-hosts-ip-address-from-in-a-container

--- bpro.sh
+++ bpro.sh
@@ -8,7 +8,7 @@ if [ -n "$(git status --porcelain)" ]; then
   TAG=${TAG}-dr
 fi
 CONTAINER_HOSTNAME=buildpro_${TAG}
-XARG="--volume=/tmp/.X11-unix:/tmp/.X11-unix --env=DISPLAY=${DISPLAY}"
+XARG="--env=DISPLAY=${DISPLAY}"
 while getopts ":c:m:r:t:x" opt
 do
   case ${opt} in
@@ -26,18 +26,15 @@ do
       CONTAINER_HOSTNAME=buildpro_${TAG}
       ;;
     x )
-      CONTAINER_DISPLAY="0"
-      DISPLAY_NUM=$(echo $DISPLAY | cut -d. -f1 | cut -d: -f2)
-      AUTH_COOKIE=$(xauth list | grep "^$(hostname)/unix:${DISPLAY_NUM} " | awk '{print $3}')
-      rm -rf .display
-      mkdir -p .display/socket
-      touch .display/Xauthority
-      xauth -f .display/Xauthority add ${CONTAINER_HOSTNAME}/unix:${CONTAINER_DISPLAY} \
-        MIT-MAGIC-COOKIE-1 ${AUTH_COOKIE}
-      socat TCP4:localhost:60${DISPLAY_NUM} UNIX-LISTEN:.display/socket/X${CONTAINER_DISPLAY} &
-      XARG="--volume=$(pwd)/.display/socket:/tmp/.X11-unix \
-        --volume=$(pwd)/.display/Xauthority:/home/${USER}/.Xauthority \
-        --env=DISPLAY=:${CONTAINER_DISPLAY}" # 172.17.0.1
+      DOCKER_HOST=$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+')
+      DISPLAY_SCREEN=$(echo $DISPLAY | cut -d: -f2)
+      DISPLAY_NUM=$(echo ${DISPLAY_SCREEN} | cut -d. -f1)
+      MAGIC_COOKIE=$(xauth list ${DISPLAY} | awk '{print $3}')
+      XAUTH=/tmp/.docker.xauth
+      touch ${XAUTH}
+      xauth -f ${XAUTH} add ${DOCKER_HOST}:${DISPLAY_NUM} . ${MAGIC_COOKIE}
+      XARG="--env=DISPLAY=${DOCKER_HOST}:${DISPLAY_SCREEN}
+        --volume=${XAUTH}:${XAUTH} --env=XAUTHORITY=${XAUTH}"
       ;;
     \? )
       echo "Invalid option: $OPTARG" 1>&2
@@ -54,6 +51,7 @@ docker container run \
   --volume=$(pwd)/image/scripts:/scripts \
   --volume=$MOUNT:/srcdir \
   --volume="$HOME/.ssh:/home/${USER}/.ssh" \
+  --volume=/tmp/.X11-unix:/tmp/.X11-unix \
   ${XARG} \
   --user=$(id -u ${USER}):$(id -g ${USER}) \
   --hostname=${CONTAINER_HOSTNAME} \
