3020893624bc971c8f4661bc3c2a86a40209b069
Scott M Anderson <smanders@sdl.usu.edu>
2021-11-02 15:43:06 -0600

    docker-compose.sh: -c option to create _bldcontainer bundle

--- docker-compose.sh
+++ docker-compose.sh
@@ -8,23 +8,43 @@ function build
     docker-compose build
   fi
 }
+function createContainerBundle
+{
+  if [[ -d _bldcontainer ]]; then
+    rm -rf _bldcontainer
+  fi
+  mkdir _bldcontainer
+  if [[ -x .devcontainer/denv.sh ]]; then
+    ./.devcontainer/denv.sh
+  fi
+  if [[ -x _bldcontainer/create.sh ]]; then
+    ./_bldcontainer/create.sh
+    ls -l _bldcontainer
+    du -sh _bldcontainer
+  fi
+}
 function usage
 {
   echo "`basename -- $0` usage:"
   echo " -h      display this help message"
   echo "         run the build container (no switches)"
   echo " -b      build docker image(s)"
+  echo " -c      create _bldcontainer bundle"
 }
 if [ $# -eq 0 ]; then
   build
   docker-compose run --rm bpro
   exit 0
 fi
-while getopts "bh" opt
+while getopts "bch" opt
 do
   case ${opt} in
     b )
       build
+      exit 0
+      ;;
+    c )
+      createContainerBundle
       exit 0
       ;;
     h )
