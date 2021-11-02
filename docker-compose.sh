2e4501a04ae53429bf0a14df8f4cdbf0cb89a8f7
Scott M Anderson <smanders@sdl.usu.edu>
2021-11-02 15:37:39 -0600

    docker-compose.sh: -b option to build image(s)

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,8 +1,39 @@
 #!/usr/bin/env bash
 cd "$( dirname "$0" )"
-if  [[ -x .devcontainer/denv.sh ]]; then
-  ./.devcontainer/denv.sh
-  cat .env
-  docker-compose build
+function build
+{
+  if  [[ -x .devcontainer/denv.sh ]]; then
+    ./.devcontainer/denv.sh
+    cat .env
+    docker-compose build
+  fi
+}
+function usage
+{
+  echo "`basename -- $0` usage:"
+  echo " -h      display this help message"
+  echo "         run the build container (no switches)"
+  echo " -b      build docker image(s)"
+}
+if [ $# -eq 0 ]; then
+  build
   docker-compose run --rm bpro
+  exit 0
 fi
+while getopts "bh" opt
+do
+  case ${opt} in
+    b )
+      build
+      exit 0
+      ;;
+    h )
+      usage
+      exit 0
+      ;;
+    \? )
+      usage
+      exit 0
+      ;;
+  esac
+done
