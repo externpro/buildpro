1b5bcb2e21a69a7e4df28671609f42b06b853b11
Scott M Anderson <smanders@sdl.usu.edu>
2021-12-29 11:22:16 -0700

    add compose.gpu.[sh|yml]
    issue https://github.com/smanders/buildpro/issues/11

---
+++ .devcontainer/compose.gpu.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+cd "$( dirname "$0" )"
+source ./.devcontainer/funcs.sh
+function usage
+{
+  echo "`basename -- $0` usage:"
+  echo " -h      display this help message"
+  echo "         run the build container (no switches)"
+  echo " -b      build docker image(s)"
+  echo " -c      create offline container bundle"
+  echo " -g      GPU container"
+}
+if [ $# -eq 0 ]; then
+  composereq
+  init
+  docker-compose --profile pbld build
+  docker-compose run --rm bld
+  exit 0
+fi
+while getopts "bcgh" opt
+do
+  case ${opt} in
+    b )
+      composereq
+      pvreq
+      init
+      docker-compose --profile pbld build
+      exit 0
+      ;;
+    c )
+      pvreq
+      createContainerBundle
+      exit 0
+      ;;
+    g )
+      gpureq
+      composereq
+      init
+      docker-compose --profile pgpu build
+      docker-compose run --rm gpu
+      exit 0
+      ;;
+    h )
+      usage
+      exit 0
+      ;;
+    \? )
+      usage
+      exit 0
+      ;;
+  esac
+done
