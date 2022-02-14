1b15aadb5b67d48aae9c561c7cd898ec059d06e6
Scott M Anderson <smanders@sdl.usu.edu>
2022-02-14 10:01:03 -0700

    compose.bld.sh: remove -c option
    * -c option: create offline container bundle
    * offline container bundles are exclusively for plugin builds now
    * plugins should use compose.gpu.sh, which offers -g (GPU container)

--- .devcontainer/compose.bld.sh
+++ .devcontainer/compose.bld.sh
@@ -7,7 +7,6 @@ function usage
   echo " -h      display this help message"
   echo "         run the build container (no switches)"
   echo " -b      build docker image(s)"
-  echo " -c      create offline container bundle"
 }
 if [ $# -eq 0 ]; then
   composereq
@@ -16,18 +15,13 @@ if [ $# -eq 0 ]; then
   docker-compose run --rm bld
   exit 0
 fi
-while getopts "bch" opt
+while getopts "bh" opt
 do
   case ${opt} in
     b )
       composereq
       init
       docker-compose --profile pbld build
-      exit 0
-      ;;
-    c )
-      offlinereq
-      createContainerBundle
       exit 0
       ;;
     h )
