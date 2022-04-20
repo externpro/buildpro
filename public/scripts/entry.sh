ba484fbdb5968ed087a90f3e218aea8ef79ed0f6
Scott M Anderson <smanders@sdl.usu.edu>
2022-04-20 11:22:38 -0600

    compose: working_dir
    entry script no longer does 'cd /bpvol'
    entry script simplified to just running bash

--- public/scripts/entry.sh
+++ public/scripts/entry.sh
@@ -1,10 +1,2 @@
 #!/usr/bin/env bash
-cd /bpvol
-if [[ -z $1 ]]; then
-  /bin/bash
-elif [ $1 = "shell" ]; then
-  /bin/bash
-elif [ $1 = "build" ]; then
-  echo "performing software build"
-  gcc helloworld.c -o helloworld -Wall
-fi
+/bin/bash
