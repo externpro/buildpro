b101c40f0e8fe500ecf1dd6669de3b9d2f7e75c1
Scott M Anderson <smanders@sdl.usu.edu>
2023-07-25 17:06:14 -0600

    rocky85-pro: initial image (replace centos7-pro)
    issue https://github.com/smanders/buildpro/issues/23

--- .devcontainer/denv.sh
+++ .devcontainer/denv.sh
@@ -60,9 +60,9 @@ else
   env="${env}\nADDSRC2=_bld/*.sh"
 fi
 ##############################
-# NOTE: EXTERN_DIR and GCC_VER need to match buildpro's public/centos7-pro.dockerfile
+# NOTE: EXTERN_DIR and GCC_VER need to match buildpro's public/rocky85-pro.dockerfile
 EXTERN_DIR=/opt/extern
-GCC_VER=gcc931
+GCC_VER=gcc921
 ##############################
 function findVer
 {
