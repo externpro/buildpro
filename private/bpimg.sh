e7bd56e3fb017ae4865d5f22ce378fbb52288220
Scott M Anderson <smanders@sdl.usu.edu>
2023-08-25 17:29:44 -0600

    bpimg: gtag obsolete
    https://github.com/smanders/buildpro/issues/54

--- private/bpimg.sh
+++ private/bpimg.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 cd "$( dirname "$0" )"
-gtag=`git describe --tags`
-if [ -n "$(git status --porcelain --untracked=no)" ] || [[ ${gtag} == *"-g"* ]]; then
-  gtag=latest
-fi
-# download/pull images (ghimg.sh builds them)
 for img in centos7-run centos7-pro centos7-bld centos7-dev
 do
   dfile=../.devcontainer/${img}.dockerfile
