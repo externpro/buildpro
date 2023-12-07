33cbc54bc7291a4b705999830fbad54d7397f40d
Scott M Anderson <smanders@sdl.usu.edu>
2023-12-07 10:56:48 -0700

    rocky85: private/ directory changes
    issue https://github.com/smanders/buildpro/issues/23

--- private/bpimg.sh
+++ private/bpimg.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd "$( dirname "$0" )"
-for img in centos7-run centos7-pro centos7-bld centos7-dev
+for img in centos7-run rocky85-pro rocky85-bld rocky85-dev
 do
   dfile=../.devcontainer/${img}.dockerfile
   awk -v r="${img}" '{gsub(/%BP_REPO%/,r)} 1' bit.head.dockerfile > ${dfile}
