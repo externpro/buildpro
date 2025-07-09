1600557906a7f240e0ddebe9e1004abeb8cb6bfa
Scott M Anderson <anderson.scott.m@icloud.com>
2025-07-09 12:27:44 -0600

    ghimg.sh: simplify for development/testing
    resolves https://github.com/externpro/buildpro/issues/114

--- public/ghimg.sh
+++ public/ghimg.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
 cd "$( dirname "$0" )"
-gtag=`git describe --tags`
-if [ -n "$(git status --porcelain --untracked=no)" ] || [[ ${gtag} == *"-g"* ]]; then
-  gtag=latest
-fi
-# build ghcr.io images
+# build local/test images
 for img in ubuntu rocky-pro rocky-mdv rocky-ci rocky-pin rocky-pdv
 do
-  pkg=ghcr.io/externpro/buildpro/${img}:${gtag}
   time docker image build \
     --network=host \
-    --build-arg BPROTAG=${gtag} \
+    --build-arg BPROTAG=latest \
     --file ${img}.dockerfile \
-    --tag ghcr.io/externpro/buildpro/${img}:latest \
-    --tag ${pkg} .
-  docker push ghcr.io/externpro/buildpro/${img}:${gtag}
+    --tag buildpro/${img}:latest .
 done
