13eb07b6370ec705fef6c846a49d326f9293e890
Scott M Anderson <anderson.scott.m@icloud.com>
2025-12-11 14:46:17 -0700

    ghimg.sh: add rocky10-gcc15

--- public/ghimg.sh
+++ public/ghimg.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd "$( dirname "$0" )"
 # build local/test images
-for img in ubuntu rocky9-gcc13 rocky8-gcc9 rocky-mdv rocky-ci rocky-pin rocky-pdv
+for img in ubuntu rocky10-gcc15 rocky9-gcc13 rocky8-gcc9 rocky-mdv rocky-ci rocky-pin rocky-pdv
 do
   time docker image build \
     --network=host \
