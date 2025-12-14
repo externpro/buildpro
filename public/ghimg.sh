a0fa05dc793dea187f9165c2818b5022e3810f7a
Scott M Anderson <anderson.scott.m@icloud.com>
2025-12-14 01:08:03 -0700

    public/ghimg: optional arg(s) to specify image(s) to build

--- public/ghimg.sh
+++ public/ghimg.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 cd "$( dirname "$0" )"
 # build local/test images
-for img in ubuntu rocky10-gcc15 rocky9-gcc13 rocky8-gcc9 rocky-mdv rocky-ci rocky-pin rocky-pdv
+if [ "$#" -gt 0 ]; then
+  images=("$@")
+else
+  images=(ubuntu rocky10-gcc15 rocky9-gcc13 rocky8-gcc9 rocky-mdv rocky-ci rocky-pin rocky-pdv)
+fi
+for img in "${images[@]}"
 do
   time docker image build \
     --network=host \
